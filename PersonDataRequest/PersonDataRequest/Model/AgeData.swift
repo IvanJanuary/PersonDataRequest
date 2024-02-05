0833d1dfcfc96d3263bd33ed129f1d1797a46205
Ivan <>
2024-02-05 12:20:27 +0200

    Create AgeData and GenderData models

---
+++ PersonDataRequest/PersonDataRequest/Model/AgeData.swift
@@ -0,0 +1,12 @@
+//
+//  AgifyData.swift
+//  PersonDataRequest
+//
+//  Created by Ivan on 02.02.2024.
+//
+
+import Foundation
+
+struct AgeData: Decodable {
+    let age: Int?
+}
---
+++ PersonDataRequest/PersonDataRequest/Model/GenderData.swift
@@ -0,0 +1,13 @@
+//
+//  PersonData.swift
+//  PersonDataRequest
+//
+//  Created by Ivan on 02.02.2024.
+//
+
+import Foundation
+
+struct GenderData: Decodable {
+    let gender: String?
+    let probability: Int?
+}
