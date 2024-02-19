67d911e0601af753c54a6f271749bf6372147249
Ivan <>
2024-02-19 16:41:39 +0200

    Use Result type to handle errors in ApiHelper (Result<T?, Error>)

--- PersonDataRequest/PersonDataRequest/Controller/ApiHelper.swift
+++ PersonDataRequest/PersonDataRequest/Controller/ApiHelper.swift
@@ -9,30 +9,30 @@ import Foundation
 
 struct ApiHelper {
     
-    func makeRequest<T: Decodable>(urlString: String, t: T.Type, completion: @escaping (T?) -> Void) {
+    func makeRequest<T: Decodable>(urlString: String, t: T.Type, completion: @escaping (Result<T?, Error>) -> Void) {
         guard let url = URL(string: urlString) else {
-            completion(nil)
+            completion(.failure(NSError(domain: "Invalid URL", code: 0, userInfo: nil)))
             return
         }
         
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
             if let error = error {
                 print("Gender request error: \(error.localizedDescription)")
-                completion(nil)
+                completion(.failure(error))
                 return
             }
             
             guard let data = data else {
-                completion(nil)
+                completion(.failure(NSError(domain: "No Data", code: 0, userInfo: nil)))
                 return }
             
             
             do {
                 let decodedData = try JSONDecoder().decode(T.self, from: data)
-                completion(decodedData)
+                completion(.success(decodedData))
             } catch {
                 print("Decoding error JSON age: \(error)")
-                completion(nil)
+                completion(.failure(error))
             }
         }
         task.resume()
