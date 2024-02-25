ce91816e8b3968e7aa69e39e4e2ea1600779115a
Ivan <>
2024-02-25 13:45:20 +0200

    In DataViewController
    Add activityLabel outlet
    Create activityUrl constant and assign a URL to it
    Сall the makeRequest function
    Create activityUpdateUI function

--- PersonDataRequest/PersonDataRequest/Controller/DataViewController.swift
+++ PersonDataRequest/PersonDataRequest/Controller/DataViewController.swift
@@ -15,7 +15,7 @@ class DataViewController: UIViewController {
     @IBOutlet weak var genderLabel: UILabel!
     @IBOutlet weak var probabilityLabel: UILabel!
     @IBOutlet weak var ageLabel: UILabel!
-    
+    @IBOutlet weak var activityLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +28,7 @@ class DataViewController: UIViewController {
         guard let selectedName = selectedName else { return }
         let genderUrl = "https://api.genderize.io/?name=\(selectedName)"
         let ageUrl = "https://api.agify.io/?name=\(selectedName)"
+        let activityUrl = "https://www.boredapi.com/api/activity/"
         
         let apiHelper = ApiHelper()
         
@@ -51,6 +52,20 @@ class DataViewController: UIViewController {
                 guard let ageData = ageData else { return }
                 DispatchQueue.main.async {
                     self?.ageUpdateUI(with: ageData)
+                }
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    self?.errorAlert(with: error)
+                }
+            }
+        }
+        
+        apiHelper.makeRequest(urlString: activityUrl, t: ActivityData.self) { [weak self] result in
+            switch result {
+            case .success(let activityData):
+                guard let activityData = activityData else { return }
+                DispatchQueue.main.async {
+                    self?.activityUpdateUI(with: activityData)
                 }
             case .failure(let error):
                 DispatchQueue.main.async {
@@ -85,6 +100,10 @@ class DataViewController: UIViewController {
         } else {
             ageLabel.text = "Age unknown"
         }
+    }
+    
+    func activityUpdateUI(with activity: ActivityData) {
+        activityLabel.text = "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum. Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum"
     }
 }
         
