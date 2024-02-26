2ccdbe2c5b65a93a3c9e9e6a6b3510c53cef36fb
Ivan <>
2024-02-26 14:59:05 +0200

    in DataViewController
    Remove test text
    Return text to string from query

--- PersonDataRequest/PersonDataRequest/Controller/DataViewController.swift
+++ PersonDataRequest/PersonDataRequest/Controller/DataViewController.swift
@@ -103,7 +103,7 @@ class DataViewController: UIViewController {
     }
     
     func activityUpdateUI(with activity: ActivityData) {
-        activityLabel.text = "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum. Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum"
+        activityLabel.text = activity.activity
     }
 }
         
