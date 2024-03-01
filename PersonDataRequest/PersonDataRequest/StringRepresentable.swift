4ea32a1b692ad5cdb1d228773ac972c02822fee7
Ivan <>
2024-03-01 13:21:39 +0200

    Create StringRepresentatable protocol

---
+++ PersonDataRequest/PersonDataRequest/StringRepresentable.swift
@@ -0,0 +1,14 @@
+//
+//  StringRepresentable.swift
+//  PersonDataRequest
+//
+//  Created by Ivan on 29.02.2024.
+//
+
+import Foundation
+
+protocol StringRepresentable {
+    var stringRepresentation: String { get }
+    
+    
+}
